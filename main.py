dada370023b13c8628ae5b0da239d38362acbc91
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-21 22:28:35 +0600

    pedmas

---
+++ main.py
@@ -0,0 +1,17 @@
+#1
+x = int(input("Enter here : "))
+y = int(input("Enter here : "))
+z = int(input("Enter here : "))
+p = int(input("Enter here : "))
+q = int(input("Enter here : "))
+
+res = x**2+(p+q-y)/z
+print("The result is ",res)
+
+#2
+name = str(input("Enter your name here : "))
+
+if name == "Taseen":
+    print("You are the right person.")
+else:
+    print("Who are you?")
